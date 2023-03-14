3d52fdd22e179ca772e9b8a283739a3e5c36357e
Elliott Stenvall <Elst1127@storsjogymnasiet.se>
2023-03-14 10:36:25 +0100

    En till sebbe

--- Frukter.py
+++ Frukter.py
@@ -4,11 +4,34 @@ looping = True
 
 def print_fruit(userinput):
     fnr = int(userinput)
-    print("\n" + Frukter[fnr-1 + "kommer här! \n"])
+    print("\n" + Frukter[fnr-1] + "kommer här! \n")
+
 
 while looping:
+
+    print("---------------------------------------")
+    print("\n-:fruktAutomat.-\n")
+    i=1
+
+
+
+    for frukt in Frukter:
+        print(str(i) + ": " + frukt)
+        i+=1
+
+    frukter = input("\nMata in nr på frukt du vill ha: ")
+
+    print_fruit(frukter)
+
+
+
     go = input("vill du välja en frukt till? j/n ")
     print("\n")
 
     if(go == "n"):
         break
+
+    print("-------------------------")
+    print("Du får en frukt till")
+    slumpfrukt = random.randint(1, 5)
+    print_fruit(slumpfrukt)
